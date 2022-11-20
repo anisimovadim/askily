1508ce67f36ba7f85f6f5c16ad2b72a08e6d1b7c
anisimovadim <anisimov.vadim.alexeevich@mail.ru>
2022-11-20 19:12:42 +0300

    apdate

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-let a = document.getSelection("details")
+let a = document.getElementById("imgD1")
+let b = document.getElementById("item_flex1")
+let a1 = document.getElementById("imgD2")
+let b1 = document.getElementById("item_flex2")
+let a2 = document.getElementById("imgD3")
+let b2 = document.getElementById("item_flex3")
+let a3 = document.getElementById("imgD4")
+let b3 = document.getElementById("item_flex4")
+let a4 = document.getElementById("imgD5")
+let b4 = document.getElementById("item_flex5")
+let a5 = document.getElementById("imgD6")
+let b5 = document.getElementById("item_flex6")
+let a6 = document.getElementById("imgD7")
+let b6 = document.getElementById("item_flex7")
+
+
+b.onclick = function (){
+    a.classList.toggle("openD")
+    console.log(a)
+}
+b1.onclick = function (){
+    a1.classList.toggle("openD")
+    console.log(a1)
+}
+b2.onclick = function (){
+    a2.classList.toggle("openD")
+    console.log(a2)
+}
+b3.onclick = function (){
+    a3.classList.toggle("openD")
+    console.log(a3)
+}
+b4.onclick = function (){
+    a4.classList.toggle("openD")
+    console.log(a4)
+}
+b5.onclick = function (){
+    a5.classList.toggle("openD")
+    console.log(a5)
+}
+b6.onclick = function (){
+    a6.classList.toggle("openD")
+    console.log(a6)
+}
